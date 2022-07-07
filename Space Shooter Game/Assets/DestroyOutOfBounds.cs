ee5d39bc077567c79f77262bc4051d4ad5a1015c
Trey Davis <swattray@gmail.com>
2022-07-07 17:43:11 -0600

    Unit 6 lab

---
+++ Space Shooter Game/Assets/DestroyOutOfBounds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float toBounds = 30.0f;
+
+    public float lowerBounds = -10.0f;
+
+
+   void Awake()
+   {
+    //Time.timeScale = 1;
+   }
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > toBounds)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBounds)
+        {
+            Debug.log("Game Over");
+            Destroy(gameObject);
+            //Time.timeScale = 0;
+        }
+    }
+}
---
+++ Space Shooter Game/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+
+    public float speed = 25;
+
+    public float xRange = 30;
+
+    public Transform blaster;
+
+    public GameObject lazerBolt;
+    
+    // Update is called once per frame
+    void Update()
+    {
+      horizontalInput = Input.GetAxis("Horizontal");
+
+      transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+      if(transform.position.x < -xRange)
+      {
+        transform.position = new Vector3(-xRange,transform.position.y, transform.position.z);
+      }  
+
+      if(transform.position.x > xRange)
+      {
+        transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
+      }
+
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+        Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+      }
+
+    }
+    private void OnTriggerEnter(Collider other)
+      {
+        Destroy(other.gameObject);
+      }
+    
+}
