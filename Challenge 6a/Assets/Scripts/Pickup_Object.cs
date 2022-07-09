638b5ce11970d1059f6d3c79bc523c4a80beb08b
Trey Davis <swattray@gmail.com>
2022-07-09 17:26:05 -0600

    Challenge 6a

---
+++ Challenge 6a/Assets/Scripts/Pickup_Object.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Object : MonoBehaviour
+{
+    public float Object;
+    public float Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player = Object)
+        {
+            Object.transform.postion;
+        }
+    }
+}
